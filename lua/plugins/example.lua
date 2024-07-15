220d54ec1455dce6c9e167556addcf92806df6e6
SadiqOnGithub <sadiqonemail@gmail.com>
2024-07-15 23:45:51 +0530

    plugin and tutor

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,21 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+if true then return {
+ {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end,
+  },
+  {
+    'tpope/vim-abolish',
+    event = "VeryLazy",
+  },
+  {
+    "mbbill/undotree",
+    event = "VeryLazy",
+  }
+} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
